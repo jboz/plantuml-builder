974894fb8ab165173fb4c8ec93e6af5b056e8f7c
jboz <julienboz@gmail.com>
2017-05-03 18:55:55 +0200

    release config

---
+++ deploy/publish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    exit $?
+fi
