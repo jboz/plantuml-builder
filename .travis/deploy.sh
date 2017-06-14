b24d5a67ed20b18b26d343d5598bc3fe11b86b60
jboz <julienboz@gmail.com>
2017-06-15 00:03:19 +0200

    make release v0.1

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -25,14 +25,10 @@ fi
 
 echo "reading project version..."
 PROJECT_VERSION=`mvn -q exec:exec -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive`
-if [[ $? != 0 || ! $PROJECT_VERSION ]]; then
-    echo "failed to parse project version"
-    return 1
-fi
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
     echo "deploying version to maven centrale..."
-    if ! mvn deploy --settings .travis/settings.xml -DperformRelease=true -DskipTests=true -B -U -X; then
+    if ! mvn deploy --settings .travis/settings.xml -DperformRelease=true -DskipTests=true -B -U; then
         echo "maven deploy failed"
         exit 1
     fi
@@ -43,22 +39,19 @@ if [ "$MAKE_RELEASE" = 'true' ]; then
     git config user.email "travis-ci@ifocusit.ch"
     GIT_TAG=v$PROJECT_VERSION
     echo "create git tag $GIT_TAG"
-    if ! git tag "$GIT_TAG" -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"; then
-        echo "failed to create git tag"
-        exit 1
-    fi
+    git tag "$GIT_TAG" -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
 
     echo "preparing next version..."
-    if ! mvn -q build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}-SNAPSHOT versions:commit; then
-        echo "set next dev version failed"
-        exit 1
-    fi
+    mvn -q build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}-SNAPSHOT versions:commit
+
     NEXT_VERSION=`mvn -q exec:exec -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive`
     echo "next development version will be $NEXT_VERSION"
 
+    # Commit the "changes", i.e. the new version.
+    git add -A .
     git commit -m "set next development version to $NEXT_VERSION"
     
     REPO=`git config remote.origin.url`
-    echo "pushing new development version to $TRAVIS_REPO_SLUG ($REPO)..."
-    git push --tags "https://jboz:$OAUTH_TOKEN@github.com/jboz/plantuml-builder.git"
+    echo "pushing new development version..."
+    git push --tags "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 fi
